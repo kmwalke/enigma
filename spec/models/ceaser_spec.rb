48ccd7ca193743cfebcdba1b0a5fa06e2928b998
Kent <kentslaymaker@gmail.com>
2025-05-26 14:42:34 -0600

    additional tests

--- spec/models/ceaser_spec.rb
+++ spec/models/ceaser_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Ceaser, type: :model do
-  let(:encoder) { Ceaser.new }
+  let(:encoder) { Ceaser.new(shift: 1) }
   let(:message) { 'my message' }
   let(:encoded_message) { encoder.encode(message) }
+
+  it 'creates an encoder' do
+    c = Ceaser.new(shift: 13)
+
+    expect(c.shift).to eq(13)
+  end
 
   it 'encodes a message' do
     expect(encoder.encode(message)).not_to eq(message)
