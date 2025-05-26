37ede746f65cc10f0578103013349ac6220d96fd
Kent <kentslaymaker@gmail.com>
2025-05-26 16:42:12 -0600

    rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,11 +12,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_25_205103) do
+ActiveRecord::Schema[8.0].define(version: 20_250_525_205_103) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
+  enable_extension 'pg_catalog.plpgsql'
 
-  create_table "ceasers", force: :cascade do |t|
-    t.integer "shift"
+  create_table 'ceasers', force: :cascade do |t|
+    t.integer 'shift'
   end
 end
--- spec/models/ceaser_spec.rb
+++ spec/models/ceaser_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Ceaser, type: :model do
-  let(:ceaser13) { Ceaser.new(shift: 13) }
-  let(:ceaser_8) { Ceaser.new(shift: -8) }
+  let(:ceaser13) { described_class.new(shift: 13) }
+  let(:ceaser_neg8) { described_class.new(shift: -8) }
   let(:message) { 'my message' }
   let(:encoded_message13) { ceaser13.encode(message) }
-  let(:encoded_message_8) { ceaser_8.encode(message) }
+  let(:encoded_message_neg8) { ceaser_neg8.encode(message) }
 
   it 'encodes a message' do
     expect(ceaser13.encode(message)).not_to eq(message)
