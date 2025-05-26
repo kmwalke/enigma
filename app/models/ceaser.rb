b1a4f0a902de60281c79a41a18de5311da4f5683
Kent <kentslaymaker@gmail.com>
2025-05-26 17:31:01 -0600

    refactor

--- app/models/ceaser.rb
+++ app/models/ceaser.rb
@@ -1,15 +1,33 @@
 class Ceaser < ApplicationRecord
   include Encoder
+  FORWARD = 1
+  BACK = -1
 
   def encode(message)
-    message.chars.map do |c|
-      (c.ord + shift).chr
-    end.join.to_s
+    adjust_chars(message, FORWARD)
   end
 
   def decode(message)
+    adjust_chars(message, BACK)
+  end
+
+  private
+
+  def adjust_chars(message, direction)
     message.chars.map do |c|
-      (c.ord - shift).chr
+      if direction < 0
+        decrement_char(c)
+      elsif direction > 0
+        increment_char(c)
+      end
     end.join.to_s
+  end
+
+  def increment_char(c)
+    (c.ord + shift).chr
+  end
+
+  def decrement_char(c)
+    (c.ord - shift).chr
   end
 end
