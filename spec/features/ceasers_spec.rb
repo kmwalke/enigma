da8ff2f1fcba877a0cb157148dc8e4a2744428a6
Kent <kentslaymaker@gmail.com>
2025-06-11 19:16:53 -0600

    update design spec

--- spec/features/ceasers_spec.rb
+++ spec/features/ceasers_spec.rb
@@ -31,13 +31,13 @@ RSpec.feature 'Ceasers' do
       # Just adding a number to all allowed ruby chars is causing issues
       # 2] pry(main)> c.encode 'The quick red fox jumped over the lazy, brown dog.'
       # => "]qn)z~rlt){nm)ox\x81)s~vynm)x\x7Fn{)}qn)uj\x83\x825)k{x\x80w)mxp7"
-      # the \x81 and \x7 s, etc are causing problems.
+      # the \x81 and \x7, etc are causing problems.
       # Create an encoder map
-      # This would be an array of allowed chars  SEEMS THAT ONLY LETTERS ARE ALLOWED
+      # This would be an array of allowed chars  SEEMS THAT ONLY LETTERS ARE ALLOWED IN THE ORIGINAL ENIGMA
       # Add Error handling for non allowed chars
       # Preferably the same chars that the enigma used
       # Instead of just adding a ruby int to a ruby char, access the index+encoding_value position of the encoding map
-      # This would just be an array of all allowable characters, overflows loop to the begining
+      # This would just be an array of all allowable characters, overflows loop to the beginning
       # Use a class, CharacterMap or something, that all encoders can use
       # CharacterMap.characters = [a,b,c,d,...]
       # use two hashes to encode/decode
@@ -49,6 +49,12 @@ RSpec.feature 'Ceasers' do
       # 'message'.chars.each do |c|
       #    hash_2[hash_1[c]+shift] // don't forget that overflows roll over somehow
       # end.merge
+      # Random Ceaser (Enigma wheels) could use a third hash that maps two numbers together
+      #   {1:4, 12:2, etc} for all combos up to 26
+      #   use this mapping instead of adding 'shift" to the char
+      
+      # In the future, I GUESS, if there is a way to convert all unicode into intergers (which is certainly doable), this is extendable to all unicode
+      #   This sounds like a pain in the ass, though
       expect(true).to be(false)
     end
   end
